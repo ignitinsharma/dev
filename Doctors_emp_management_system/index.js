b31e1d2bf5b2078836b4c6622c9ccf365e81f3f9
nitin <nitinsharma754010@gmail.com>
2022-07-11 16:55:27 +0530

    price-management

---
+++ Doctors_emp_management_system/index.js
@@ -0,0 +1,76 @@
+
+// depkoy link-> 
+// here we select form 
+//then we put event lis on submit forms's submit button so then enter query come here
+let form=document.querySelector("form");
+form.addEventListener("submit",myfun);
+let arr=[];
+function myfun(){
+    event.preventDefault();
+    // i created this obj for capturing the values from form and stored in this object 
+    let obj={
+        name:document.querySelector("#name").value,
+        id:document.querySelector("#docID").value,
+        dept:document.querySelector("#dept").value,
+        expe:document.querySelector("#exp").value,
+        email:document.querySelector("#email").value,
+        mobile:document.querySelector("#mbl").value,
+    }
+
+    arr.push(obj);
+
+
+    display(arr)
+
+
+    function display(arr){
+    //empty because we dont want previous data again and again
+     document.querySelector("tbody").innerHTML="";
+     arr.forEach(function (el) {
+        //create a table row for storing the table column data
+        let row=document.createElement("tr");
+        let col1=document.createElement("td");
+        let col2=document.createElement("td");
+        let col3=document.createElement("td");
+        let col4=document.createElement("td");
+        let col5=document.createElement("td");
+        let col6=document.createElement("td");
+        let col7=document.createElement("td");
+        let col8=document.createElement("td");
+       
+         //append column into main table row
+        row.append(col1,col2,col3,col4,col5,col6,col7,col8);
+
+        //append main table row into table body
+        document.querySelector("tbody").append(row);
+        col1.innerText=el.name;
+        col2.innerText=el.id;
+        col3.innerText=el.dept;
+        col4.innerText=el.expe;
+
+        //this constion will check expirence and give output to 
+         //column 7 section
+        if(el.expe>5){
+            col7.innerText="Senior"
+        }
+        else if(el.expe>1 && el.expe<5){
+            col7.innerText="Junior"
+        }
+        else{
+            col7.innerText="Trainee"
+        }
+        col5.innerText=el.email;
+        col6.innerText=el.mobile;
+        
+        //create event listener when someone click on delete ....
+        //for removing particular row by clicking on button
+        col8.innerText="Delete"
+
+        //change delete button color
+        col8.style.color="red";
+        col8.addEventListener("click",function (){
+            event.target.parentNode.remove()
+        })
+     });
+    }
+}
---
+++ Price_managment-system/index.js
@@ -0,0 +1,87 @@
+
+document.querySelector("form").addEventListener("submit", myfun);
+
+
+let arr=[];
+function myfun(){
+    event.preventDefault();
+     let obj={
+        pname:document.querySelector("#name").value,
+        category:document.querySelector("#category").value,
+        brand:document.querySelector("#brand").value,
+        prange:document.querySelector("#price").value,
+        delivery:document.querySelector("#deliveredBy").value,
+     };
+
+     arr.push(obj);
+
+    mainfun(arr);
+
+    function mainfun(arr){
+        document.querySelector("tbody").innerHTML="";
+
+        arr.forEach(function(el){
+            
+            let row= document.createElement("tr");
+            let col1=document.createElement("td");
+            let col2=document.createElement("td");
+            let col3=document.createElement("td");
+            let col4=document.createElement("td");
+            let col5=document.createElement("td");
+            let col6=document.createElement("td");
+            let col7=document.createElement("td");
+            // let col9=document.createElement("td");
+            
+            document.querySelector("tbody").append(row)
+            row.append(col2,col1,col3,col4,col5,col6,col7);
+            
+
+            col1.innerText=el.category;
+            col2.innerText=el.pname;
+            col3.innerText=el.brand;
+            col4.innerText=el.prange;
+            col5.innerText=el.delivery;
+            
+            if(el.prange>1000){
+                return col6.innerText="Expensive";
+            }
+            else if(el.prange<1000){
+                return col6.innerText="Not-Expensive";
+            }
+            
+            col7.innerText="delete"
+            col7.addEventListener("click", function out(){
+                event.target.parentNode.remove()
+            });
+
+            
+
+            if(col3.innerText=="" || col2.innerText=="" || col1.innerText=="" || col4.innerText=="" || col5.innerText==""){
+                alert("Fill all input fields");
+                document.querySelector("tbody").innerHTML="";
+            }
+
+
+            
+
+            
+            
+        })
+       
+        
+        
+    }
+
+    let sum=0;
+    for(let i=0; i<=arr.length; i++){
+        sum+= Number(arr[i].prange);
+
+    }
+    document.querySelector("#total-price").innerHTML="Total Price:-" + " "+ sum;
+
+
+}
+
+
+
+
