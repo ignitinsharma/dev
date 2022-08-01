2b453ba4dc694d0f5f9ae440f4f335a4e686af1c
nitin <nitinsharma754010@gmail.com>
2022-08-02 00:02:56 +0530

    ..

--- Admin_panel_system_with_notes/scripts/inventory.js
+++ Admin_panel_system_with_notes/scripts/inventory.js
@@ -1,1 +1,38 @@
 let maindata=JSON.parse(localStorage("products")) || [];
+
+
+function display(maindata){
+
+    maindata.foreach(function(el){
+
+        let img=document.createElement("img");
+        img.innerText=el.image;
+
+        let type=document.createElement("h2");
+        type.innerText=el.type;
+
+        let price=document.createElement("h3");
+        price.innerText=el.price;
+
+        let desc=document.createElement("p")
+        desc.innerText=el.desc;
+
+        let removebtn=document.createElement("button");
+        removebtn.innerText="Remove";
+        removebtn.setAttribute("id", "remove_button")
+
+        removebtn.addEventListener('click', function(){
+            deleteitem(index)
+        })
+    
+        document.getElementById("all_products").append(img,type,price,desc)
+    
+    })
+}
+
+
+function deleteitem(ind){
+    maindata.splice(ind,1);
+    display(maindata);
+    localStorage.setItem('products', JSON.stringify(maindata))
+}
