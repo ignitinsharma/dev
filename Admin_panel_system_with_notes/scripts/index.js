a521a16dbd50bf6c9619a876f729cac82679c3c9
nitin <nitinsharma754010@gmail.com>
2022-08-01 23:39:36 +0530

    ....

--- Admin_panel_system_with_notes/scripts/index.js
+++ Admin_panel_system_with_notes/scripts/index.js
@@ -1,7 +1,10 @@
+
+
 let arr=JSON.parse(localStorage.getItem("products")) || [];
 
 
-    //---> constructor function for making obj blueprint 
+    // step-- 2
+    //---> create constructor function for making obj blueprint or also storing the user data in obj one by one
     //--> prevously we use obj but know we can use that and then push form into that function
 
     function Productsfun(type, desc, price, image) {
@@ -12,11 +15,11 @@ let arr=JSON.parse(localStorage.getItem("products")) || [];
     }
 
 
-
+    // step-- 1
     //---> this is for taking data from form --- firstly we take data into obj and then store them into array but know
     //---> we can use constructor function for making obj
 
-    function productsdata(){
+    function productsdata(event){
 
         event.preventDefault();
         let form=document.getElementById("products");
@@ -24,15 +27,24 @@ let arr=JSON.parse(localStorage.getItem("products")) || [];
         let desc=form.desc.value;
         let price=form.price.value;
         let image=form.image.value;
+    
+        
+        // step-- 3
+        //---> here we send the data into constructor function for creating
+        //--> objects for per data enter by users
+        let arrdata= new Productsfun(type, desc, price, image)
 
 
-        //---> here we send the data into constructor
-        let data= new Productsfun(form, type, desc, price, image)
+        //--> pushing that into array because we wanna store multiple users data their thats why
+        arr.push(arrdata);
+
+
+        //step-- 4 
+        //---> semding data to local storage 
+        localStorage.setItem("products", JSON.stringify(arr))
+
 
     }
+
     
-
-//       arr.push(products);
-
-//       JSON.stringify(localStorage("products", arr));
-
+    
---
+++ Admin_panel_system_with_notes/scripts/inventory.js
@@ -0,0 +1,1 @@
+let maindata=JSON.parse(localStorage("products")) || [];
