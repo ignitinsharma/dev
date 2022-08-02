63a3f3b56e107f98bccde0fc4fe5ac21bf4f6b72
nitin <nitinsharma754010@gmail.com>
2022-08-02 12:38:41 +0530

    .....

--- Admin_panel_system_with_notes/scripts/inventory.js
+++ Admin_panel_system_with_notes/scripts/inventory.js
@@ -5,11 +5,18 @@ let maindata=JSON.parse(localStorage.getItem("products")) || [];
 
 
 //step-- 2
-//--> take display function and use as that 
+//--> take display function and use as that for display data after performing that all operataion we call it with passing all data;
 function display(arr){
 
-    arr.forEach(function(el,index){
+    //-->when user enter data new data old cant replace and both will be stored together
+    document.getElementById("all_products").innerHTML=""
 
+    //step-- 3
+    //--> we apply forEach to that main array because we wanna perfomre opeation on that data we take form index.js page
+    //--> then we create div or things according to which we wanna show data to  users;
+    arr.forEach(function(el,ind){
+
+       
         let maindiv=document.createElement("div");
 
         let img=document.createElement("img");
@@ -24,24 +31,40 @@ function display(arr){
         let desc=document.createElement("p")
         desc.innerText=el.desc;
 
+        //--> create button for deleting things    
         let removebtn=document.createElement("button");
         removebtn.innerText="Remove";
+
+        //--> setting id to button  first we take id then next id name ec remove_button
         removebtn.setAttribute("id", "remove_button")
 
+         //--> then put event lisner on button when someone click what happend then
         removebtn.addEventListener('click', function(){
-            deleteitem(index)
+
+            //--> we pass index here because deletion function delete those vale when use click
+            deleteitem(ind)
         })
         
-        maindiv.append(img,type,price,desc)
+         //--> here we are appending all the things we created and build into the
+          //--> main div and then main container 
+        maindiv.append(img,type,price,desc,removebtn)
         document.getElementById("all_products").append(maindiv)
     
     })
 }
 
+//--> calling main fucntion for showing things 
 display(maindata);
 
+
+//--> delete function here we will pass index and then put splice on maindata and 
+//--> splice(index(you wanna delete), 1 ( value how much delete starting from that index))
 function deleteitem(ind){
     maindata.splice(ind,1);
-    display(maindata);
+
+    //--> then send updated data to local stoage 
     localStorage.setItem('products', JSON.stringify(maindata))
+    
+    //--> then call the function again for latest updated value to to the user
+    display(maindata);
 }
